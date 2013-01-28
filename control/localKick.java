15698bcdbca4739e2db4ef932af91b9f9f80d37b
Michael Western <s1036976@hoddam.inf.ed.ac.uk>
2013-01-28 12:31:56 +0000

    A local kick file

---
+++ control/localKick.java
@@ -0,0 +1,42 @@
+import lejos.nxt.Motor;
+
+public class localKick {
+
+	public final Motor KICKER = Motor.A;
+
+	private volatile boolean isKicking = false;
+	
+	public void kickControl() {
+
+	KICKER.smoothAcceleration(false);
+        KICKER.regulateSpeed(false);
+
+	if (isKicking) {
+            return;
+        }
+
+        isKicking = true;
+
+	KICKER.forward();	
+	//KICKER.rotateTo(0);        
+	try {
+		Thread.sleep(80);
+	} catch (Exception e) {}
+	KICKER.setSpeed(900);
+        KICKER.resetTachoCount();
+        KICKER.backward();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(250);
+                } catch (InterruptedException e) {
+                    //
+                }                
+		KICKER.rotateTo(0);
+                isKicking = false;
+            }
+        }).start();
+    }
+}
